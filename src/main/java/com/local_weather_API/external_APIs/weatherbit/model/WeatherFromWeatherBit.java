8acb74a996a5dd8a4780a5dfff033f8b2ee2b504
Pawel Iskra <52103903+Pawel-Iskra@users.noreply.github.com>
2020-03-19 20:45:37 +0100

    Update WeatherFromWeatherBit.java

--- src/main/java/com/local_weather_API/external_APIs/weatherbit/model/WeatherFromWeatherBit.java
+++ src/main/java/com/local_weather_API/external_APIs/weatherbit/model/WeatherFromWeatherBit.java
@@ -1,6 +1,8 @@
 package com.local_weather_API.external_APIs.weatherbit.model;
 
-public class WeatherFromWeatherBit {
+import com.local_weather_API.business_logic.transformations.WeathersConditions;
+
+public class WeatherFromWeatherBit implements WeathersConditions {
     
     private Data[] data;
 
@@ -13,5 +15,35 @@ public class WeatherFromWeatherBit {
 
     public void setData(Data[] data) {
         this.data = data;
+    }
+
+    @Override
+    public float getTemperature() {
+        return getData()[0].getTemp();
+    }
+
+    @Override
+    public float getTemperatureSensed() {
+        return getData()[0].getAppTemp();
+    }
+
+    @Override
+    public int getPressure() {
+        return (int) getData()[0].getPres();
+    }
+
+    @Override
+    public int getHumidity() {
+        return getData()[0].getHumidity();
+    }
+
+    @Override
+    public float getWindSpeed() {
+        return getData()[0].getWindSpeed();
+    }
+
+    @Override
+    public int getWindDegrees() {
+        return getData()[0].getWindDir();
     }
 }
